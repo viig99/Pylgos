08d44e8374029a2f616032ddb290edd1f51db903
Arjun Variar <accio.arjun@gmail.com>
2012-11-04 21:53:47 +0530

    Commiting karatsuba with gauss' trick

---
+++ gauss_multiplication.py
@@ -0,0 +1,19 @@
+
+def gauss(x,y,b):
+	lx,ly = len(str(x))/2 , len(str(y))/2
+	m = lx if lx < ly else ly
+	if max(x,y) < b**m or max(x,y) < 1000:
+		return x*y
+	bm = b**m
+	b2m = b**(2*m)
+	x1 = x / bm
+	x0 = x % bm
+	y1 = y / bm
+	y0 = y % bm
+	z2 = gauss(x1,y1,b)
+	z0 = gauss(x0,y0,b)
+	z1 = gauss(x1+x0,y1+y0,m)-z2-z0
+	return (z2*b2m) + (z1*bm) + z0
+	
+if __name__ == "__main__":
+	print(gauss(234534,456712,16))
